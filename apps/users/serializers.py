e3303ab470a56b43c57415ca2839335dd2ac0324
Sasha Nosyk <acomely2014@gmail.com>
2025-12-03 14:54:11 +0200

    fix_users

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -7,3 +7,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ('id', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = UserModel(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
