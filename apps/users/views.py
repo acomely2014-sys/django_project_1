a82d15704f561c0cb2d9b6011775be8c5ec1b8f4
Sasha Nosyk <acomely2014@gmail.com>
2025-12-17 19:38:30 +0200

    add_get_meview

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from apps.users.serializers import UserSerializer
 
@@ -7,3 +8,13 @@ from apps.users.serializers import UserSerializer
 # Create your views here.
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
+
+
+class MeView(GenericAPIView):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        serializer = self.serializer_class(user)
+        return Response(serializer.data)
--- config/settings.py
+++ config/settings.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # 'rest_framework_simplejwt.tokens_blacklist',
+    # 'corsheaders',
     # myAPPS
     'core',
     'apps.products',
     'apps.users',
     # 'apps.auth',
+
 
 ]
 
@@ -55,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'config.urls'
